e26fe5b46b347c1033ef8bda36f728e4338c7814
Joe Brown <joebrown.4576@gmail.com>
2023-03-11 22:31:47 +0000

    Fix badge alignment

--- components/Badge.vue
+++ components/Badge.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="d-flex flex-column align-center">
-    <img :src="imageUrl" style="width: 100px" />
+    <img :src="imageUrl" style="height: 100px" />
     <p class="text-center font-weight-bold mt-5">
       <slot />
     </p>
