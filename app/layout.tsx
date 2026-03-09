907e9aa73e10ed48850ea8bea1bae2c3d4627649
Joe Brown <joebrown.4576@gmail.com>
2026-03-09 18:47:30 +0000

    add posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/common/footer";
 import { Navbar } from "@/components/common/navbar";
 import { cn } from "@/lib/utils";
+import { PostHogProvider } from "@/providers/posthog-provider";
 import { ReviewCountProvider } from "@/providers/review-count-provider";
 import type { Metadata } from "next";
 import { DM_Sans, Inter } from "next/font/google";
@@ -34,11 +35,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
       className={cn(dmSans.variable, inter.variable, "scroll-smooth")}
     >
       <body className="font-sans antialiased">
-        <ReviewCountProvider>
-          <Navbar />
-          {children}
-          <Footer />
-        </ReviewCountProvider>
+        <PostHogProvider>
+          <ReviewCountProvider>
+            <Navbar />
+            {children}
+            <Footer />
+          </ReviewCountProvider>
+        </PostHogProvider>
       </body>
     </html>
   );
---
+++ providers/posthog-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider as PhProvider } from "posthog-js/react";
+import { ReactNode, useEffect } from "react";
+
+type PostHogProviderProps = {
+  children: ReactNode;
+};
+
+export const PostHogProvider = ({ children }: PostHogProviderProps) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      return;
+    }
+
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      defaults: "2026-01-30",
+    });
+  }, []);
+
+  return <PhProvider client={posthog}>{children}</PhProvider>;
+};
