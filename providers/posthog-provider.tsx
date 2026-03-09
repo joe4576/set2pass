6f768379a8b0f13c89f4f9bf5fe16e957d4a4bab
Joe Brown <joebrown.4576@gmail.com>
2026-03-09 19:12:40 +0000

    enable cookieless mode

--- providers/posthog-provider.tsx
+++ providers/posthog-provider.tsx
@@ -17,6 +17,7 @@ export const PostHogProvider = ({ children }: PostHogProviderProps) => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       defaults: "2026-01-30",
+      cookieless_mode: "always",
     });
   }, []);
 
